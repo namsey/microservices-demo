c4cfd342e79a38ad38c242667d1615e68dec98b8
namsey <namansingh48@gmail.com>
2025-08-18 17:25:28 +0530

    modified jenkinsfile.adservice and buildApp file

--- jenkins/shared-lib/vars/buildApp.groovy
+++ jenkins/shared-lib/vars/buildApp.groovy
@@ -1,17 +1,12 @@
 def call(Map args = [:]) {
   def service = args.service
-  if (fileExists("${service}/build.gradle")) {
-    dir(service) {
-      sh './gradlew build -x test'
-    }
-  } else if (fileExists("${service}/package.json")) {
-    dir(service) {
-      sh 'npm ci && npm run build'
-    }
-  } else if (fileExists("${service}/go.mod")) {
-    dir(service) {
-      sh 'go build -o main .'
-    }
+
+  if (fileExists("build.gradle")) {
+    sh './gradlew build -x test'
+  } else if (fileExists("package.json")) {
+    sh 'npm ci && npm run build'
+  } else if (fileExists("go.mod")) {
+    sh 'go build -o main .'
   } else {
     error "No known build tool found in ${service}"
   }
